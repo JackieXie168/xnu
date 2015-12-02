d144295e145f992ff4c7fd5d64b57d82c9f4a63d
William Kent <wjk@sunsol.me>
2015-12-01 20:19:35 -0500

    Add extra safety check to build script

--- SETUP/xcodescripts/copy-kernel-executable.sh
+++ SETUP/xcodescripts/copy-kernel-executable.sh
@@ -2,7 +2,7 @@ if [ -e "${DSTROOT}/System/Library/Kernels/kernel" ]; then
     cp -f "${DSTROOT}/System/Library/Kernels/kernel" "${DSTROOT}/mach_kernel"
 elif [ -e "${DSTROOT}/System/Library/Kernels/kernel.development" ]; then
     cp -f "${DSTROOT}/System/Library/Kernels/kernel.development" "${DSTROOT}/mach_kernel"
-else
+elif [ ! -e "${DSTROOT}/mach_kerne;" ]; then
     echo "Kernel file not found" 1>&2
     exit 1
 fi
