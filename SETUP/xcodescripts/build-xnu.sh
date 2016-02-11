e5318d622675287a103c1ff867e8f4ae1ccefc69
William Kent <wjk@sunsol.me>
2016-02-10 23:34:24 -0500

    Add code to build script
    This version of xnu installs its kernel into a
    location other than the one expected by the booter.
    This change copies it back so that the booter can
    find the kernel when it is appropriate.

--- SETUP/xcodescripts/build-xnu.sh
+++ SETUP/xcodescripts/build-xnu.sh
@@ -1,5 +1,12 @@
+set -e
+
 mkdir -p "${XNU_INSTALL_BASE}"
 mkdir -p "${DERIVED_FILE_DIR}/xnu.obj"
 
 make install "DSTROOT=${XNU_INSTALL_BASE}" "OBJROOT=${DERIVED_FILE_DIR}/xnu.obj" "CTFCONVERT=${BUILT_PRODUCTS_DIR}/ctfconvert" "CTFMERGE=${BUILT_PRODUCTS_DIR}/ctfmerge" "BUILD_LTO=0" &> $XNU_BUILD_LOG_FILE
-exit $?
+
+if [ -f "${XNU_INSTALL_BASE}/System/Library/Kernels/kernel.development" ]; then
+    cp -f "${XNU_INSTALL_BASE}/System/Library/Kernels/kernel.development" "${XNU_INSTALL_BASE}/mach_kernel"
+elif [ -f "${XNU_INSTALL_BASE}/System/Library/Kernels/kernel" ]; then
+    cp -f "${XNU_INSTALL_BASE}/System/Library/Kernels/kernel" "${XNU_INSTALL_BASE}/mach_kernel"
+fi
